cd621877f977bf77f535874f1db849664f663855
spctrm404 <46218112+spctrm404@users.noreply.github.com>
2024-08-03 20:54:00 +0900

    switch is finished

--- src/components/Switch/Switch.jsx
+++ src/components/Switch/Switch.jsx
@@ -1,11 +1,5 @@
-import {
-  useCallback,
-  useContext,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-} from 'react';
-import usePointerInteraction from '../../hooks/usePointerInteraction.js';
+import { useCallback, useContext, useRef } from 'react';
+import { Switch as AriaSwitch } from 'react-aria-components';
 import { ThemeContext } from '../../context/ThemeContext.jsx';
 import st from './_Switch.module.scss';
 import classNames from 'classnames/bind';
@@ -14,55 +8,38 @@ const cx = classNames.bind(st);
 
 const Switch = ({
   state: value = false,
-  disabled = false,
-  onChange = null,
-  className = null,
+  onChange = () => {},
+  className = '',
+  ...props
 }) => {
   const { theme } = useContext(ThemeContext);
 
-  const rootDomRef = useRef(null);
-  const handleDomRef = useRef(null);
+  const rootRef = useRef(null);
 
-  const onPointerClickHandler = useCallback(() => {
-    onChange?.(!value);
-  }, [value, onChange]);
-
-  const switchPI = usePointerInteraction();
-  useEffect(() => {
-    switchPI.setTargetRef(rootDomRef.current);
-    switchPI.setOnPointerClick(onPointerClickHandler);
-  }, [switchPI, onPointerClickHandler]);
-  const handlePI = usePointerInteraction();
-  useEffect(() => {
-    handlePI.setTargetRef(handleDomRef.current);
-    handlePI.setOnPointerClick(onPointerClickHandler);
-  }, [handlePI, onPointerClickHandler]);
-
-  useLayoutEffect(() => {
-    switchPI.setDisabled(disabled);
-    handlePI.setDisabled(disabled);
-  }, [switchPI, handlePI, disabled]);
+  const onChangeHandler = useCallback(
+    (newValue) => {
+      onChange?.(newValue);
+    },
+    [onChange]
+  );
 
   return (
-    <div
-      className={`${cx('switch')} ${className || ''}`}
-      ref={rootDomRef}
+    <AriaSwitch
+      className={cx('switch', 'switch__root', className)}
+      value={value}
+      onChange={onChangeHandler}
       data-theme={theme}
-      data-value={value}
-      data-state={
-        switchPI.getState() === 'pressed' ? 'pressed' : handlePI.getState()
-      }
+      ref={rootRef}
+      {...props}
     >
-      <div className={cx('switch__shape', 'switch-shape')} />
-      <div
-        className={cx('switch__handle', 'switch-handle')}
-        ref={handleDomRef}
-        tabIndex={disabled ? -1 : 0}
-      >
-        <div className={cx('switch__handle__state', 'switch-handle-state')} />
-        <div className={cx('switch__handle__shape', 'switch-handle-shape')} />
+      <div className={cx('switch__track', className)}>
+        <div className={cx('switch__track__shape', className)} />
+        <div className={cx('switch__thumb', className)}>
+          <div className={cx('switch__state', className)} />
+          <div className={cx('switch__thumb__shape', className)} />
+        </div>
       </div>
-    </div>
+    </AriaSwitch>
   );
 };
 
